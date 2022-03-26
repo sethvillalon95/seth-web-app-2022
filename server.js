a0b5c526eed9324cb647073d5d24a1c1951a1468
Seth Villalon <sethvillalon95@gmail.com>
2022-03-25 23:22:08 -1000

    Install MongoDB on local to resolve bug

--- server.js
+++ server.js
@@ -1,5 +1,28 @@
-const express = require("express");
-const app = express();
+require('dotenv').config();
 
-const portNum = 3000;
-app.listen(portNum, ()=> console.log("Listening to port: "+ portNum));
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+//connect to the db
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser:true,
+    useUnifiedTopology: true
+}).catch(err=>{
+    console.log(Error,err.message)
+    console.log('error comming from the catch statement')
+}).then(()=>{
+    console.log('DB connected successfully!')
+    const portNum = process.env.PORTNUM;
+    app.listen(portNum, ()=> console.log('Listening to port: '+ portNum));
+
+}
+
+)
+
+// const db = mongoose.connection;
+// db.on('error',(error)=> console.log(error));
+// db.once('open',()=>console.log('Connected to DB'));
+
+
+
