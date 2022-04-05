c1993b608016c857a54cc298938ca31f978f9f42
Seth Villalon <sethvillalon95@gmail.com>
2022-04-04 23:51:27 -1000

    Finish Create One, Get One,Get All and Middleware

--- routes/items.js
+++ routes/items.js
@@ -1,14 +1,55 @@
-const express = require('express')
+const express = require('express');
+const { route } = require('express/lib/application');
 const router =express.Router();
+const Item = require('../models/item')
 
-router.get('/', (req,res)=> {
-    res.send("Hello World")    
+//get all
+router.get('/', async (req,res)=>{
+    try {
+        const items = await Item.find()
+        //sends all the items object as a json
+        res.json(items)
+    } catch (err) {
+        res.status(500).json({messsage:err.messsage})
+    }
 })
+//get one 
+router.get('/:id', getItem,(req,res)=>{
+    res.json(res.item)
+})
+//create one
+router.post('/', async(req,res)=>{
+    const item = new Item({
+        item_name: req.body.item_name,
+        item_type: req.body.item_type
+    })
+    try {
+        const newItem =await item.save()
+        res.status(200).json(newItem)
+    } catch (err) {
+        res.status(400).json({message:err.message})
+    }
+})
+//update one
 
+//delete one
 
-// async function getItem(req,res,next){
-//     let item
-//     try{
-//     }
-// }
+// 
+async function getItem(req,res,next){
+    let item
+    try {
+        item = await Item.findById(req.params.id)
+        if(item==null){
+            // object not found
+            return res.status(404).json({message:'Cannot find item'})
+        }
+    } catch (error) {
+        return res.status(500).json({message:err.message})
+
+    }
+    res.item=item
+    next()
+
+}
+
 module.exports=router
